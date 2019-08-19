3da230700bf4239ba3d8f2769f839aa06d2dcc52
Miguel Ramírez <Miguel@Mac-mini-de-user.local>
2019-08-19 10:55:52 -0500

    Se configuro el login y se cargo los usurios con los Roles que pueden acceder al sistema.

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class LoginController extends AbstractController
 {
     /**
-     * @Route("/", name="login")
+     * @Route("/login", name="login")
      */
     public function index(AuthenticationUtils $authenticationUtils)
     {
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,16 +1,53 @@
 <?php
 
 namespace App\DataFixtures;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
+    private $encoder;
+    
+    public function __construct(UserPasswordEncoderInterface $encoder)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {   
+        //Se creo el usuario cajero
+        $user = new User();
+        $user->setUsername('cajero');
+        $user->setPassword(
+            $this->encoder->encodePassword($user, 'cajero123')
+        );
+        $user->setRole('ROLE_CAJERO');
+        $user->setEmail('themike123work@gmail.com');
+        $manager->persist($user);        
+
+        //Se creo el usuario supervisor
+        $user = new User(); 
+        $user->setUsername('supervisor');
+        $user->setPassword(
+            $this->encoder->encodePassword($user, 'supervisor123')
+        );
+        $user->setRole('ROLE_SUPERVISOR');
+        $user->setEmail('themike123work@gmail.com');
+        $manager->persist($user);        
+
+        //Se creo el usuario supervisor
+        $user = new User(); 
+        $user->setUsername('admin');
+        $user->setPassword(
+            $this->encoder->encodePassword($user, 'admin123')
+        );
+        $user->setRole('ROLE_ADMIN');
+        $user->setEmail('themike123work@gmail.com');
+        $manager->persist($user);
+
 
         $manager->flush();
     }
